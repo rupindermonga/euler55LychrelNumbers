af955529a03b21f8a9886dd8bc53c585f42dcc82
Rupinder <rupindermonga@gmail.com>
2020-02-13 14:50:41 +0530

    Lychre Numbers

---
+++ euler55LychrelNumbers.py
@@ -0,0 +1,51 @@
+'''
+If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.
+
+Not all numbers produce palindromes so quickly. For example,
+
+349 + 943 = 1292,
+1292 + 2921 = 4213
+4213 + 3124 = 7337
+
+That is, 349 took three iterations to arrive at a palindrome.
+
+Although no one has proved it yet, it is thought that some numbers, like 196, never produce a palindrome. A number that never forms a palindrome through the reverse and add process is called a Lychrel number. Due to the theoretical nature of these numbers, and for the purpose of this problem, we shall assume that a number is Lychrel until proven otherwise. In addition you are given that for every number below ten-thousand, it will either (i) become a palindrome in less than fifty iterations, or, (ii) no one, with all the computing power that exists, has managed so far to map it to a palindrome. In fact, 10677 is the first number to be shown to require over fifty iterations before producing a palindrome: 4668731596684224866951378664 (53 iterations, 28-digits).
+
+Surprisingly, there are palindromic numbers that are themselves Lychrel numbers; the first example is 4994.
+
+How many Lychrel numbers are there below ten-thousand?
+
+NOTE: Wording was modified slightly on 24 April 2007 to emphasise the theoretical nature of Lychrel numbers.
+'''
+
+import math 
+   
+# the recursive function to reverse 
+def rev(num):
+    return int(num != 0) and ((num % 10) * (10**int(math.log(num, 10))) + rev(num // 10))
+
+def isPalindromic(n):
+    # n is a number
+    return str(n) == str(n)[::-1]
+
+def LychrelNumbers(n,m):
+    #Till n numbers to check and m iterations allowed to check
+    count = 0
+    for i in range(n+1):
+        number = i
+        j = 0
+        while j < m:
+            number += rev(number)
+            if isPalindromic(number):
+                break
+            elif isPalindromic(number + rev(number)):
+                break
+            else:
+                j += 1
+                
+        if j == m:
+            count += 1
+    return count
+
+final = LychrelNumbers(10000, 50)
+print(final)      
